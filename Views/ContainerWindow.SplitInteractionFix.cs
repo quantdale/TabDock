ed8bdd0619c7683ecad82a365214405306c530d3
Michael Dale Palaca <93045129+quantdale@users.noreply.github.com>
2026-08-14 21:44:23 +0800

    fix: compile split interaction diagnostics

--- Views/ContainerWindow.SplitInteractionFix.cs
+++ Views/ContainerWindow.SplitInteractionFix.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using TabDock.Models;
+using TabDock.Services;
 using TabDock.ViewModels;
 
 namespace TabDock.Views;
